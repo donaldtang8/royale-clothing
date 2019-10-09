bdf0b1baa15a3bbd5a554e3bff4f7b58282cb3cb
turazi <dtang4@stevens.edu>
2019-10-09 16:06:35 -0400

    Added home page

---
+++ src/components/menuItem/MenuItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "../../scss/menuItem.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">Show Now</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
