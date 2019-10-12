ac6dfae39a206469f4e0d49d5d53efa6edbc4db2
turazi <dtang4@stevens.edu>
2019-10-12 16:16:34 -0400

    Changed middleware settings

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,10 @@ import rootReducer from "./RootReducer";
 
 const initialState = {};
 
-const middlewares = [logger];
+const middlewares = [];
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 export const store = createStore(
   rootReducer,
