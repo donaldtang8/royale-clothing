84d77e7ccb859c887c8360b92af46d71e2235c4b
turazi <dtang4@stevens.edu>
2019-10-18 04:29:52 -0400

    Added compression library for heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
+const compression = require("compression");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -11,6 +12,8 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // cors - cross origin request, checks that no exterior front end is trying to access our server
