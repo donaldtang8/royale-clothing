449225245f037ec9591d1ffd08ddb9eb66816624
turazi <dtang4@stevens.edu>
2019-10-18 19:17:14 -0400

    Added PWA compliant code, https redirect

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 const compression = require("compression");
+const enforce = require("express-sslify");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -16,6 +17,7 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 // cors - cross origin request, checks that no exterior front end is trying to access our server
 app.use(cors());
 
@@ -40,6 +42,10 @@ app.use("/payment", (req, res) => {
       res.status(200).send({ success: stripeRes });
     }
   });
+});
+
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
 });
 
 app.listen(port, error => {
